749e6e25b6f7d29fbe42bd22340d9344d3b4fac9
tomo1227 <tomo.soccer1227@gmail.com>
2024-11-16 20:51:11 +0000

    langタグを修正

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="jp">
+    <html lang="ja">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
